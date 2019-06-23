2e65d450499c613a93c6572f30f5c1aad41129e1
Deshswat <deshmukh.swati12@gmail.com>
2019-06-23 01:52:32 -0700

    2 files modified

--- index.js
+++ index.js
@@ -20,10 +20,41 @@ for (let i = 0; i < acc.length; i++) {
 
 const inputElements = document.getElementsByTagName("input");
 for (let j = 0; j < inputElements.length; j++) {
-  inputElements[j].addEventListener("change", handleInputChange);
+    inputElements[j].addEventListener("change", handleInputChange); 
 }
 
 function handleInputChange() {
   const element = document.getElementById(this.name);
   element.innerHTML = this.value;
 }
+
+function validateEmail() {
+      var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      email = document.getElementById("email").value;
+      if(mailFormat.test(email.value) == false) 
+      { 
+        alert("You have entered an invalid email address!"); 
+        return false;
+      } 
+      else 
+      { 
+        alert("Thank you, Email Address Valid");
+        return true; 
+      }
+}
+
+function validateName(){ 
+      var name = document.getElementById("fullName").value,
+          nameFormat = /^[A-Za-z]+$/;
+      if(nameFormat.test(name.value) == false) 
+      {
+        alert("You have entered an invalid FullName!"); 
+        return true;
+      }
+      else
+      {
+        alert('FullName must have alphabet characters only');
+        name.focus();
+        return false;
+      }
+  }
